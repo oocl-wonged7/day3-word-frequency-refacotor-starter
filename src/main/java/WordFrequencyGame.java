f18aae968b35e47722928c656349e9e73bde1a35
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-20 15:48:58 +0800

    refactor: rename constant and add constant for space

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,11 +3,12 @@ import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
 
-    public static final String SPACE = "\\s+";
+    public static final String SPACE_REGEX = "\\s+";
     public static final String LINE_BREAK = "\n";
+    public static final String SPACE = " ";
 
     public String getWordFrequency(String sentence) {
-        if (sentence.split(SPACE).length == 1) {
+        if (sentence.split(SPACE_REGEX).length == 1) {
             return sentence + " 1";
         } else {
             try {
@@ -22,19 +23,20 @@ public class WordFrequencyGame {
 
     private static String joinResult(List<WordFrequency> wordFrequencies) {
         return wordFrequencies.stream()
-                .map(wordFrequency -> wordFrequency.getWord() + " " + wordFrequency.getWordCount()).collect(Collectors.joining(LINE_BREAK))
+                .map(wordFrequency -> wordFrequency.getWord() + SPACE + wordFrequency.getWordCount()).collect(Collectors.joining(LINE_BREAK))
                 .toString();
     }
 
     private List<WordFrequency> getWordFrequencies(List<WordFrequency> wordFrequencies) {
         Map<String, List<WordFrequency>> wordToWordFrequencies = getWordToWordFrequencies(wordFrequencies);
         return wordToWordFrequencies.entrySet().stream()
-                .map(wordToWordFrequencyEntry -> new WordFrequency(wordToWordFrequencyEntry.getKey(), wordToWordFrequencyEntry.getValue().size())).sorted((word, nextWord) -> nextWord.getWordCount() - word.getWordCount())
+                .map(wordToWordFrequencyEntry -> new WordFrequency(wordToWordFrequencyEntry.getKey(), wordToWordFrequencyEntry.getValue().size()))
+                .sorted((word, nextWord) -> nextWord.getWordCount() - word.getWordCount())
                 .toList();
     }
 
     private static List<WordFrequency> getInitialWordFrequencies(String sentence) {
-        String[] words = sentence.split(SPACE);
+        String[] words = sentence.split(SPACE_REGEX);
         return Arrays.stream(words)
                 .map(word -> new WordFrequency(word, 1))
                 .toList();
