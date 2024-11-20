a94274d8a6bea654bee5915988de8f487c94d405
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-20 14:13:19 +0800

    refactor: optimize import and remove empty line

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,46 +1,29 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
-import java.io.CharArrayWriter;
-
-import java.time.LocalDateTime;
+import java.util.*;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
-
-
         if (inputStr.split("\\s+").length == 1) {
             return inputStr + " 1";
         } else {
-
             try {
-
                 //split the input string with 1 to n pieces of spaces
                 String[] arr = inputStr.split("\\s+");
-
-
 //                create a input list from the arr
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
                     Input input = new Input(s, 1);
                     inputList.add(input);
                 }
-
                 //get the map for the next step of sizing the same word
                 Map<String, List<Input>> map = getListMap(inputList);
-
                 List<Input> list = new ArrayList<>();
                 for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
                     Input input = new Input(entry.getKey(), entry.getValue().size());
                     list.add(input);
                 }
                 inputList = list;
-
 //                sort the list base on the frequency
                 inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
                 StringJoiner joiner = new StringJoiner("\n");
                 for (Input w : inputList) {
                     String s = w.getValue() + " " + w.getWordCount();
@@ -48,13 +31,10 @@ public class WordFrequencyGame {
                 }
                 return joiner.toString();
             } catch (Exception e) {
-
-
                 return "Calculate Error";
             }
         }
     }
-
 
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> map = new HashMap<>();
@@ -68,10 +48,6 @@ public class WordFrequencyGame {
                 map.get(input.getValue()).add(input);
             }
         }
-
-
         return map;
     }
-
-
 }
